588ba8c851befccfd955964344d08b62596d4390
Hågen Hasle <haagenhasle@gmail.com>
2023-01-19 17:07:51 +0100

    Ryddet litt, nå fungerer det som det skal

--- src/main/kotlin/no/nav/tiltakspenger/scheduler/jobs/HourlyPassageOfTimeEventsPublishingJob.kt
+++ src/main/kotlin/no/nav/tiltakspenger/scheduler/jobs/HourlyPassageOfTimeEventsPublishingJob.kt
@@ -7,7 +7,6 @@ import org.quartz.Job
 import org.quartz.JobExecutionContext
 import java.time.LocalDateTime
 import java.time.ZoneId
-import java.util.Date
 
 
 private val log = KotlinLogging.logger {}
@@ -21,35 +20,19 @@ class HourlyPassageOfTimeEventsPublishingJob(
 ) : Job {
     override fun execute(context: JobExecutionContext) {
         log.info { "Executing HourlyPassageOfTimeEventsPublishingJob" }
+        val localDateTime = findScheduledTime(context)
+        hourlyEventsGenerator.generateEventsFor(localDateTime)
+    }
+
+    private fun findScheduledTime(context: JobExecutionContext): LocalDateTime {
         log.info { "LocalDateTime.now(): ${LocalDateTime.now()}" }
-        val startTime = context.trigger.startTime // TODO: Det viser seg at denne ikke er korrekt dessverre!
-        log.info { "context.trigger.starttime $startTime" }
-
-        try {
-            val nextFireTime = context.trigger.nextFireTime
-            log.info { "nextFireTime $nextFireTime" }
-            val subsequentFireTime = context.trigger.getFireTimeAfter(nextFireTime);
-            log.info { "subsequentFireTime $subsequentFireTime" }
-            val interval = subsequentFireTime.time - nextFireTime.time;
-
-            val previousFireTime = context.trigger.previousFireTime
-            log.info { "previousFireTime $previousFireTime" }
-
-            val thisFireTime = Date(nextFireTime.time - interval)
-            log.info { "thisFireTime $thisFireTime" }
-
-        } catch (t: Throwable) {
-            log.warn("Ooops", t)
-        }
-
         val scheduledFireTime = context.scheduledFireTime
         log.info { "scheduledFireTime $scheduledFireTime" }
-
         val localDateTime = LocalDateTime.ofInstant(
             scheduledFireTime.toInstant(),
             ZoneId.systemDefault()
         )
-        log.info { "thisFireTime converted $localDateTime" }
-        hourlyEventsGenerator.generateEventsFor(localDateTime)
+        log.info { "scheduledFireTime converted $localDateTime" }
+        return localDateTime
     }
 }
